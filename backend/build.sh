c95aee874e6ab5f9207bea207dabd1395fe51a19
sahilshaikh00067 <sahilshaikh1582004@gmail.com>
2026-05-15 12:27:28 +0530

    remove admin creation from build

--- backend/build.sh
+++ backend/build.sh
@@ -3,9 +3,3 @@ set -e
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py shell -c "
-from api.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create(username='admin', password='admin', role='admin', status='Active')
-    print('Admin created')
-"
