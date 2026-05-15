7aa96f526fdd35ab394a257eb709e5571e4ccb13
sahilshaikh00067 <sahilshaikh1582004@gmail.com>
2026-05-15 12:10:06 +0530

    PostgreSQL setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -88,11 +88,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
