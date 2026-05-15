6c9194c121f31eab2e9f1da1e7a466db3b691c06
sahilshaikh00067 <sahilshaikh1582004@gmail.com>
2026-05-15 12:23:08 +0530

    create admin user

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-
+set -e
 pip install -r requirements.txt
-
 python manage.py collectstatic --noinput
-
 python manage.py migrate
+python manage.py shell -c "
+from api.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create(username='admin', password='admin', role='admin', status='Active')
+    print('Admin created')
+"
